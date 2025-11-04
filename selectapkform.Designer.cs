bd4431c9ba0e3dec977139c7726e3cb4cdcf86bf
Rafig <121453235+rafigk2v9c@users.noreply.github.com>
2025-11-05 02:26:59 +0400

    Update selectapkform.Designer.cs

--- selectapkform.Designer.cs
+++ selectapkform.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace APKdevastate
+namespace APKdevastate
 {
     partial class selectapkform
     {
@@ -34,15 +34,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.surusdurmekpaneli = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.guna2ShadowPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.surusdurmekpaneli.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // guna2ShadowPanel1
@@ -50,10 +51,11 @@
             this.guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent;
             this.guna2ShadowPanel1.Controls.Add(this.yeniproyektbutton);
             this.guna2ShadowPanel1.FillColor = System.Drawing.Color.White;
-            this.guna2ShadowPanel1.Location = new System.Drawing.Point(147, 97);
+            this.guna2ShadowPanel1.Location = new System.Drawing.Point(196, 119);
+            this.guna2ShadowPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
             this.guna2ShadowPanel1.ShadowColor = System.Drawing.Color.DimGray;
-            this.guna2ShadowPanel1.Size = new System.Drawing.Size(239, 118);
+            this.guna2ShadowPanel1.Size = new System.Drawing.Size(319, 145);
             this.guna2ShadowPanel1.TabIndex = 0;
             // 
             // yeniproyektbutton
@@ -68,9 +70,10 @@
             this.yeniproyektbutton.FillColor = System.Drawing.Color.White;
             this.yeniproyektbutton.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.yeniproyektbutton.ForeColor = System.Drawing.Color.Black;
-            this.yeniproyektbutton.Location = new System.Drawing.Point(61, 43);
+            this.yeniproyektbutton.Location = new System.Drawing.Point(81, 53);
+            this.yeniproyektbutton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.yeniproyektbutton.Name = "yeniproyektbutton";
-            this.yeniproyektbutton.Size = new System.Drawing.Size(109, 31);
+            this.yeniproyektbutton.Size = new System.Drawing.Size(145, 38);
             this.yeniproyektbutton.TabIndex = 0;
             this.yeniproyektbutton.Text = "Analyze!";
             this.yeniproyektbutton.Click += new System.EventHandler(this.yeniproyektbutton_Click);
@@ -80,9 +83,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(166, 85);
+            this.label1.Location = new System.Drawing.Point(221, 105);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 24);
+            this.label1.Size = new System.Drawing.Size(53, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Start";
             // 
@@ -91,86 +95,105 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.White;
             this.label2.Font = new System.Drawing.Font("Verdana", 15.70909F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(182, 9);
+            this.label2.Location = new System.Drawing.Point(243, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(176, 29);
+            this.label2.Size = new System.Drawing.Size(199, 32);
             this.label2.TabIndex = 4;
             this.label2.Text = "APKdevastate";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(205, 38);
+            this.label3.Location = new System.Drawing.Point(273, 47);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.Size = new System.Drawing.Size(151, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "advanced .apk analyzer";
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::APKdevastate.Properties.Resources.icons8_exit_button_24;
-            this.pictureBox2.Location = new System.Drawing.Point(516, 0);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(21, 21);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::APKdevastate.Properties.Resources.Untitled_design;
-            this.pictureBox1.Location = new System.Drawing.Point(208, 221);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(109, 95);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            // 
             // surusdurmekpaneli
             // 
+            this.surusdurmekpaneli.Controls.Add(this.label4);
             this.surusdurmekpaneli.Controls.Add(this.pictureBox3);
+            this.surusdurmekpaneli.Controls.Add(this.pictureBox1);
             this.surusdurmekpaneli.Dock = System.Windows.Forms.DockStyle.Fill;
             this.surusdurmekpaneli.Location = new System.Drawing.Point(0, 0);
+            this.surusdurmekpaneli.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.surusdurmekpaneli.Name = "surusdurmekpaneli";
-            this.surusdurmekpaneli.Size = new System.Drawing.Size(537, 317);
+            this.surusdurmekpaneli.Size = new System.Drawing.Size(716, 390);
             this.surusdurmekpaneli.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(633, 370);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "version 1.4";
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::APKdevastate.Properties.Resources.icons8_minimize_48;
-            this.pictureBox3.Location = new System.Drawing.Point(489, 0);
+            this.pictureBox3.Location = new System.Drawing.Point(652, 0);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(21, 21);
+            this.pictureBox3.Size = new System.Drawing.Size(28, 26);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 7;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::APKdevastate.Properties.Resources.Untitled_design;
+            this.pictureBox1.Location = new System.Drawing.Point(277, 270);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::APKdevastate.Properties.Resources.icons8_exit_button_24;
+            this.pictureBox2.Location = new System.Drawing.Point(688, 0);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(28, 26);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
             // selectapkform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(537, 317);
+            this.ClientSize = new System.Drawing.Size(716, 390);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.guna2ShadowPanel1);
             this.Controls.Add(this.surusdurmekpaneli);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "selectapkform";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "APKdevastate";
             this.guna2ShadowPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.surusdurmekpaneli.ResumeLayout(false);
+            this.surusdurmekpaneli.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,6 +210,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel surusdurmekpaneli;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
