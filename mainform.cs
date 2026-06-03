f2d8354f6ea0faffa86f3934c2fb26bf199e7e6e
rafok2v9c <rafiqzrbliyev@gmail.com>
2026-06-04 01:59:37 +0400

    Update mainform.cs

--- mainform.cs
+++ mainform.cs
@@ -14,7 +14,7 @@ using System.Diagnostics;
 using System.Net;
 
 //The concept and idea for the APKdevastate application belong entirely to Rafig Zarbaliyev!
-//If there is an error in the app or if you have any suggestions for additional features, please message me on Instagram(@rafok2v9c)
+//If there is an error in the app or if you have any suggestions for additional features, please message me on Instagram(@rafohub)
 //Read this guide to learn how to use APKdevastate https://rafosw.github.io/posts/apkdevastaten/
 
 namespace APKdevastate
